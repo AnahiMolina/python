f2e5d18f243a6e7c238958db095279c4c3414697
AnahiMolina <anahi_molina11@outlook.com>
2025-02-16 21:41:07 -0600

    Class python

---
+++ Class/ejemplo.py
@@ -0,0 +1,42 @@
+a = 37%4
+print (a)
+
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+
+ancho = 20
+alto = 5*9
+ancho * alto
+
+a = 14 * 2.5
+print (a)
+
+impuesto = (12.5) / 100
+precio = 100.50
+precio * impuesto
+
+#precio + _
+#round(_, 2)
+#round(14*2.78654,2)
+
+print("""\
+Selecciona una de las opciones de sexo
+ -H Hombre
+ -M Mujer
+""")
+
+nombre = "Alma"
+nombre[0]
+nombre[-1]
+
+nombre[0] == nombre[-1]
+
+
+nombre = int(input("num: "))
+print(nombre)
+
+
